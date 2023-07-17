a88fe854c49178f74660bbbeef6d4f57bef95b39
Andrii Rybak <adnrii.rybak@amomedia.com>
2023-07-17 11:22:44 +0300

    extra changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        SolarSystem earth = SolarSystem.EARTH;
+        System.out.println("Перед планетою " + earth + " знаходиться планета " + earth.prev());
+        System.out.println("Ви зараз знаходитесь на планеті " + earth);
+        System.out.println("Після планети " + earth + " знаходиться планета " + earth.next());
+        System.out.println("Планета " + earth + " - " + earth.getPositionNumberFromSun() + " від сонця планета");
+        System.out.println("Від планети " + earth + " до сонця " + earth.distanceToSun + " мільйонів кілометрів");
+        System.out.println("Радіус планети " + earth + " складає " + earth.radius + " тисяч кілометрів");
+    }
 }
--- src/main/java/SolarSystem.java
+++ src/main/java/SolarSystem.java
@@ -1,24 +1,39 @@
-import javax.swing.plaf.IconUIResource;
+public enum SolarSystem {
 
-public enum Planet {
+    MERCURY     (0, 10, null),
+    VENUS       (10, 20, MERCURY),
+    EARTH       (30, 25, VENUS),
+    MARS        (15, 60, EARTH),
+    JUPITER     (93, 22, MARS),
+    SATURN      (72, 10, JUPITER),
+    URANUS      (21, 99, SATURN),
+    NEPTUNE     (43, 34, URANUS);
 
-    private int positionNumberFromSun;
-    private int count = 1;
+    private int positionNumberFromSun = 1;
+    public final int distanceFromPreviousPlanet;
+    public final int distanceToSun;
+    public final int radius;
+    private SolarSystem prevPlanet;
+    private SolarSystem nextPlanet;
 
-    MERCURY,
-    VENUS,
-    EARTH,
-    MARS,
-    JUPITER,
-    SATURN,
-    URANUS,
-    NUPTUNE
-
-
-    Planet () {
-        positionNumberFromSun = count;
-        count++;
-
-
+    SolarSystem(int prevDistance, int radius, SolarSystem previous) {
+        this.distanceFromPreviousPlanet = prevDistance;
+        this.radius = radius;
+        this.distanceToSun = previous.distanceToSun + prevDistance;
+        positionNumberFromSun = previous.positionNumberFromSun + 1;
+        prevPlanet = previous;
+        previous.nextPlanet = this;
     }
+
+    public SolarSystem prev() {
+        return this == MERCURY ? null : prevPlanet;
     }
+
+    public SolarSystem next() {
+        return this == NEPTUNE ? null : nextPlanet;
+    }
+
+    public int getPositionNumberFromSun() {
+        return positionNumberFromSun;
+    }
+}
