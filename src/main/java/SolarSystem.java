fd391e22f08f8fa99429ef3e2d60a06601f4a0fe
Andrii Rybak <adnrii.rybak@amomedia.com>
2023-07-17 20:39:31 +0300

    Changes for Mercury

--- src/main/java/SolarSystem.java
+++ src/main/java/SolarSystem.java
@@ -1,6 +1,6 @@
 public enum SolarSystem {
 
-    MERCURY     (0, 10),
+    MERCURY     (0, 10, null),
     VENUS       (10, 20, MERCURY),
     EARTH       (30, 25, VENUS),
     MARS        (15, 60, EARTH),
@@ -9,7 +9,7 @@ public enum SolarSystem {
     URANUS      (21, 99, SATURN),
     NEPTUNE     (43, 34, URANUS);
 
-    private int positionNumberFromSun = 1;
+    private int positionNumberFromSun;
     public final int distanceFromPreviousPlanet;
     public final int distanceToSun;
     public final int radius;
@@ -19,16 +19,15 @@ public enum SolarSystem {
     SolarSystem(int prevDistance, int radius, SolarSystem previous) {
         this.distanceFromPreviousPlanet = prevDistance;
         this.radius = radius;
-        this.distanceToSun = previous.distanceToSun + prevDistance;
-        positionNumberFromSun = previous.positionNumberFromSun + 1;
-        prevPlanet = previous;
-        previous.nextPlanet = this;
-    }
-
-    SolarSystem(int prevDistance, int radius) {
-        this.distanceFromPreviousPlanet = prevDistance;
-        this.radius = radius;
-        this.distanceToSun = 50;
+        if (previous == null) {
+            this.distanceToSun = 50;
+            positionNumberFromSun = 1;
+        } else {
+            this.distanceToSun = previous.distanceToSun + prevDistance;
+            positionNumberFromSun = previous.positionNumberFromSun + 1;
+            prevPlanet = previous;
+            previous.nextPlanet = this;
+        }
     }
 
     public SolarSystem prev() {
